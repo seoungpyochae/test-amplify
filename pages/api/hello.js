df9d2f69119cba77417e7c34c1f288cb90aa1580
채승표 <seoungpyo.chae@h9works.com>
2023-01-10 18:52:57 +0900

    feat: on demand isr 샘플 추가

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  console.log("hello hander");
+
+  await res.revalidate("/");
+
+  res.status(200).json({ name: "John Doe" });
 }
